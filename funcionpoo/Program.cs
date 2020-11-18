9ea904e3021b59bfe60c7ece7eb80c994d098844
Rubenespza <70655396+Rubenespza@users.noreply.github.com>
2020-11-17 20:19:36 -0600

    Add files via upload

---
+++ funcionpoo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcionpoo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+
+            
+        {
+            Console.WriteLine("Programa de Resta");
+
+            Resta r1 = new Resta(5, 10);
+            Console.WriteLine(r1.ResultadoResta());
+            Console.ReadKey();
+        }
+    }
+}
+
+public class Resta
+{
+    //Especificacion propiedades
+    #region propiedades
+    public int var1 { get; set; }
+    public int var2 { get; set; }
+    #endregion
+    //Constructor
+    #region constructor
+    public Resta(int n1, int n2)
+    {
+        var1 = n1;
+        var2 = n2;
+    }
+    #endregion
+    //Metodo
+    #region Metodos
+    public int ResultadoResta()
+    {
+        return var1 - var2;
+    }
+    #endregion
+
+}
+ 
